1e64c33ee5484dd6ed2b8aea8934cabd94beaf55
Admin Marin <admin@marinlog.com.br>
2026-06-12 16:39:03 -0300

    log temporario cloudnary

--- backend/src/routes/chamados.js
+++ backend/src/routes/chamados.js
@@ -22,6 +22,12 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY?.slice(0, 6) + "...",
+  api_secret: process.env.CLOUDINARY_API_SECRET?.slice(0, 6) + "..."
+});
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
